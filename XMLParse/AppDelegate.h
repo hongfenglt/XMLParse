551f56585b9941fc4ce3fa7c4e9bd1d1ce11c370
勤奋的笨老头 <627999959@qq.com>
2015-08-25 10:45:45 +0800

    Initial Commit

---
+++ XMLParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMLParse
+//
+//  Created by 洪峰 on 15/8/25.
+//  Copyright (c) 2015年 洪峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMLParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMLParse
+//
+//  Created by 洪峰 on 15/8/25.
+//  Copyright (c) 2015年 洪峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
